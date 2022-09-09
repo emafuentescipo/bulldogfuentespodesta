8e8ad94848323e15f8b1d208d3d6f720baf81330
Emmanuel Fuentes Podesta <emafuentescipo@hotmail.com>
2022-09-08 21:33:46 -0300

    7

---
+++ src/components/mockData.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        id: '1',
+        title: 'corte ema',
+        price: 1000,
+        Image: 'https://img.freepik.com/foto-gratis/cliente-haciendo-corte-pelo-salon-peluqueria_1303-20861.jpg?w=2000'
+    },
+        {
+        id: '2',
+        title: 'corte facu',
+        price: 1000,
+        Image: 'https://img.freepik.com/foto-gratis/cliente-haciendo-corte-pelo-salon-peluqueria_1303-20861.jpg?w=2000'
+         },
+         {
+            id: '3',
+            title: 'corte otro',
+            price: 1000,
+            Image: 'https://img.freepik.com/foto-gratis/cliente-haciendo-corte-pelo-salon-peluqueria_1303-20861.jpg?w=2000'
+        },
+
+];
+
+export default data;
