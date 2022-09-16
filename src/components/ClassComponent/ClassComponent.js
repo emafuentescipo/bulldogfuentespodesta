4342cc24ffe230f4bee06429c29dd3ecf0c2e37e
Emmanuel Fuentes Podesta <emafuentescipo@hotmail.com>
2022-09-15 22:03:38 -0300

    8

--- src/components/ClassComponent/ClassComponent.js
+++ src/components/ClassComponent/ClassComponent.js
@@ -5,19 +5,35 @@ class ClassComponent extends Component {
     super();
     this.state = {
       initialState: 0,
+      stock: 10,
     };
   }
+
   suma = () => {
     this.setState({
       initialState: this.state.initialState + 1,
     });
   };
 
+  resta = () => {
+    this.setState({
+      initialState: this.state.initialState - 1,
+    });
+  };
+
+  stock = () => {
+    this.setState({
+      initialState: 0,
+    });
+    alert('se agoto el stock')
+  }
+
   render() {
     return (
       <div>
         <h1>{this.state.initialState}</h1>
-        <button onClick={this.suma}>Dame click</button>
+        <button onClick={this.state.initialState === 0 ? null : this.resta}>Dame click -</button>
+        <button onClick={this.state.stock <= this.state.initialState ? this.stock : this.suma}>Dame click +</button>
       </div>
     );
   }
