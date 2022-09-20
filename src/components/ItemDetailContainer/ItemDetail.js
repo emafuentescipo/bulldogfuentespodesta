09f31ba72324d94cfcf6dd0a28e78dfaa272dbe7
Emmanuel Fuentes Podesta <emafuentescipo@hotmail.com>
2022-09-20 18:58:07 -0300

    13

---
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import data from "../components/mockData";
+import { useEffect, useState } from 'react';
+import ItemList from './ItemList/ItemList';
+
+const ItemListContainer = ({greeting}) => {
+  const [productList, setProductList] = useState ([])
+
+  useEffect (() => {
+    getProducts.then((response) => {
+      setProductList(response)
+
+    });
+  }, []);
+
+  const getProducts = new Promise ((resolve, reject) => {
+      setTimeout (()=> {
+      resolve(data)
+      },2000)
+
+    });
+
+  return (
+    <>
+    <ItemList lista={productList} />
+    </>
+    );
+  };
+
+
+
+export default ItemListContainer
